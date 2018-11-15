28422f88cb1196f57801007a9a4dee101b49c28e
pogodina-nadezda <nadezda.pogodina.bio@gmail.com>
2018-11-15 13:12:40 +0300

    year became a three levels factor

--- TKM_year.R
+++ TKM_year.R
@@ -1,55 +1,52 @@
-first_filter<-read.csv("~/Bioinf_project/Autumn_project/Leukemia/data/first_filtered.csv", sep=";")
-str(data)
-
 library(lubridate)
 library(dplyr)
 
-#Считаем промежуток от ТКМ до момента последнего контакта 
-elapsed.time_2 <- first_filter$Date_alloTKM %--% first_filter$Last_contact_date
-TKM_cont <- round(as.duration(elapsed.time_2) / ddays(1))
+
+data <- read.table("data/third_filtered.csv", header = T, sep = ";")
+
+data[c(5,8,12,16:17,20:22, 24, 46, 49, 52:53, 57, 59, 61)] <- 
+  lapply(data[c(5,8,12,16:17,20:22, 24, 46, 49, 52:53, 57, 59, 61)], as.POSIXct) 
 
 #Можно начинать возиться с выживаемостью))####
 library(survival)
 library(survminer)
 
 #Соберем это в один дата фрейм для удобства
-surv_data<-data_frame(status=first_filter$Status_life, 
-                      time_life=TKM_cont/30.5,
-                      time_TKM=first_filter$Diagn.TKM_period.days./30.5,
-                      phase_do_TKM=first_filter$Phaze_do_TKM,
-                      n_TKM=first_filter$Only_one_TKM,
-                      date_TKM = first_filter$Date_alloTKM)
+surv_data<-data_frame(status=data$Status_life, 
+                      time_life=data$TKM_death/30.5)
 
-#отберем тех, у кого ТКМ была 1 раз
-surv_data<-filter(surv_data, n_TKM=="yes")
-#Уберем наблюдения с NA
-surv_data<-surv_data[complete.cases(surv_data),]
-str(surv_data)
 
-surv_data$date_TKM <- as.POSIXct(surv_data$date_TKM)
-
-#Переведем время до ТКМ в бинарный фактор через медианное значение
-surv_data <- surv_data %>% mutate(TKM_group = ifelse(as.numeric(date_TKM) <= as.numeric(as.POSIXct('2012-01-01')),
-                                                     "less", "further"))
-surv_data$TKM_group <- factor(surv_data$TKM_group)
-
-#Нам нужно рассчитать общую выживаемость в зависимости от времени до ТКМ
-# status==1 принципиальная вещь, которая стоила мне пару часов жизни)) Инвертирует то, что у врачей живой обозначен 0, а мертвый 1
+#Переведем время ТКМ в фактор
+surv_data <- surv_data %>% mutate(TKM_date_factor = ifelse(as.numeric(data$Date_alloTKM) < as.numeric(as.POSIXct('2006-01-01')),
+                                                     "2006", ifelse(as.numeric(data$Date_alloTKM) >= as.numeric(as.POSIXct('2012-01-01')),
+                                                                    "2012", "2006-2012")))
+surv_data$TKM_date_factor <- factor(surv_data$TKM_date_factor)
 
 # График по фактору "время от диагноза до ТКМ" по 12
-km_fit <- survfit(Surv(time_life, status==1) ~ TKM_group, data=surv_data, conf.type = "log-log")
+km_fit <- survfit(Surv(time_life, status=='died') ~ TKM_date_factor, data=surv_data)
 summary(km_fit, times = c(1,30,60,90*(1:10)))
 
 ggsurvplot(km_fit, data = surv_data, size = 1,  # change line size
            linetype = "strata", # change line type by groups
-           palette = c("#E7B800", "#2E9FDF"), # custom color palette
-           conf.int = TRUE, # Add confidence interval
+           pval.method = FALSE,
            pval = T,
+           pval.size=8,
            xlim = c(0, 120),
            break.y.by = 0.1,
-           break.x.by = 12,
-           cumcensor = TRUE,
-           legend = c(0.8, 0.8),
-           #legend = "bottom", 
+           cumcensor = FALSE,
+           #ggtheme = theme_minimal(),
+           risk.table.y.text.col = TRUE,
+           risk.table.y.text = FALSE,
+           test.for.trend = FALSE,
+           xlab = "Время (мес)",
+           ylab = "Кумулятивная доля выживших",
+           censor.size=9,
+           legend = c(0.8,0.9),
+           font.legend=18,
+           size=2,
+           font.x=20,
+           font.y=20,
+           font.tickslab=c(16,"plain"),
+           break.x.by = 24,
            legend.title = "Дата проведения ТКМ",
-           legend.labs = c("С 2012 года", "До 2012 года"))$plot
+           legend.labs = c("до 2006 года", "с 2006 до 2012 года", "с 2012 года"))$plot
