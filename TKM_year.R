df08b12f7c6829b93abd70b8aa7e6b8e11d701b2
pogodina-nadezda <nadezda.pogodina.bio@gmail.com>
2018-11-15 13:36:12 +0300

    added graphs without CP1 phase

--- TKM_year.R
+++ TKM_year.R
@@ -38,8 +38,8 @@ ggsurvplot(km_fit, data = surv_data, size = 1,  # change line size
            risk.table.y.text.col = TRUE,
            risk.table.y.text = FALSE,
            test.for.trend = FALSE,
-           xlab = "Время (мес)",
-           ylab = "Кумулятивная доля выживших",
+           xlab = "Время после TKM (мес)",
+           ylab = "Общая выживаемость",
            censor.size=9,
            legend = c(0.8,0.9),
            font.legend=18,
@@ -49,4 +49,49 @@ ggsurvplot(km_fit, data = surv_data, size = 1,  # change line size
            font.tickslab=c(16,"plain"),
            break.x.by = 24,
            legend.title = "Дата проведения ТКМ",
-           legend.labs = c("до 2006 года", "с 2006 до 2012 года", "с 2012 года"))$plot
+           legend.labs = c("до 2006 года", "с 2006 по 2012 год", "с 2012 года"))$plot
+
+
+
+####### Когда нет ХФ1
+data1 <- data[data$Phaze_do_TKM != "CP_1",]
+
+#Соберем это в один дата фрейм для удобства
+surv_data1 <-data_frame(status=data1$Status_life, 
+                      time_life=data1$TKM_death/30.5)
+
+
+#Переведем время ТКМ в фактор
+surv_data1 <- surv_data1 %>% mutate(TKM_date_factor = ifelse(as.numeric(data1$Date_alloTKM) < as.numeric(as.POSIXct('2006-01-01')),
+                                                           "2006", ifelse(as.numeric(data1$Date_alloTKM) >= as.numeric(as.POSIXct('2012-01-01')),
+                                                                          "2012", "2006-2012")))
+surv_data1$TKM_date_factor <- factor(surv_data1$TKM_date_factor)
+
+# График по фактору "время от диагноза до ТКМ" по 12
+km_fit1 <- survfit(Surv(time_life, status=='died') ~ TKM_date_factor, data=surv_data1)
+summary(km_fit1, times = c(1,30,60,90*(1:10)))
+
+ggsurvplot(km_fit1, data = surv_data1, size = 1,  # change line size
+           linetype = "strata", # change line type by groups
+           pval.method = FALSE,
+           pval = T,
+           pval.size=8,
+           xlim = c(0, 120),
+           break.y.by = 0.1,
+           cumcensor = FALSE,
+           #ggtheme = theme_minimal(),
+           risk.table.y.text.col = TRUE,
+           risk.table.y.text = FALSE,
+           test.for.trend = FALSE,
+           xlab = "Время после TKM (мес)",
+           ylab = "Общая выживаемость",
+           censor.size=9,
+           legend = c(0.8,0.9),
+           font.legend=18,
+           size=2,
+           font.x=20,
+           font.y=20,
+           font.tickslab=c(16,"plain"),
+           break.x.by = 24,
+           legend.title = "Дата проведения ТКМ",
+           legend.labs = c("до 2006 года", "с 2006 по 2012 год", "с 2012 года"))$plot
